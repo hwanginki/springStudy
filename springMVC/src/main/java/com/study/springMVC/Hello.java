db77f0a707009913ce07d1d981ff2d13a43be92e
every3743 <every3743@gmail.com>
2022-01-26 16:06:04 +0900

    commit

---
+++ springMVC/src/main/java/com/study/springMVC/Hello.java
@@ -0,0 +1,26 @@
+package com.study.springMVC;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+// 1. 원격 호출가능한 프로그램으로 등록
+
+@Controller
+public class Hello {
+	int iv = 10; // 인스턴스 변수
+	static int cv = 20; // static(정적) 변수
+	
+	// 2. URL과 메서드를 연결
+	@RequestMapping("/hello")
+	public void main() { // 인스턴스 메서드 - iv, cv를 둘 다 사용가능
+		System.out.println("Hello");
+		System.out.println(iv); // OK
+		System.out.println(cv); // OK
+	}
+	
+	// static 메서드 - cv만 사용가능
+	public static void main2() {
+		System.out.println(cv); // OK
+		// System.out.println(iv); // 에러
+	}
+}
